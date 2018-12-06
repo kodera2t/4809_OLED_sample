0dd4fa414a339f41af2e70b711c7a93acf3b59ab
kodera2t <kodera2t@yahoo.co.jp>
2018-12-06 15:12:30 +0900

    initial submission

---
+++ OLED_4809_sample_code.ino
@@ -0,0 +1,52 @@
+/**************************************************************************
+ This is an example for our Monochrome OLEDs based on SSD1306 drivers
+
+ Pick one up today in the adafruit shop!
+ ------> http://www.adafruit.com/category/63_98
+
+ This example is for a 128x32 pixel display using I2C to communicate
+ 3 pins are required to interface (two I2C and one reset).
+
+ Adafruit invests time and resources providing this open
+ source code, please support Adafruit and open-source
+ hardware by purchasing products from Adafruit!
+
+ Written by Limor Fried/Ladyada for Adafruit Industries,
+ with contributions from the open source community.
+ BSD license, check license.txt for more information
+ All text above, and the splash screen below must be
+ included in any redistribution.
+ **************************************************************************/
+
+//#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET -1
+Adafruit_SSD1306 display(OLED_RESET);
+
+
+void setup() {
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); 
+  display.clearDisplay();
+  display.setRotation(2);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);  
+  display.setCursor(32,0);
+  display.print("HELLO");
+    display.setCursor(32,8);
+  display.print("ATmega4809");
+    display.setCursor(32,16);
+  display.print("dev. board");
+    display.setCursor(32,24);
+  display.print("nice to see!");   
+  display.display();
+}
+
+void loop() {
+}
+
+
